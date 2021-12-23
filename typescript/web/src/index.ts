beb73663f77fd43b8bd3be688dda28c53f518ce1
ClintMI <ce@buildmidwestern.com>
2021-12-23 08:12:30 -0600

    Up to 205 on the course

---
+++ typescript/web/src/index.ts
@@ -0,0 +1,13 @@
+import { UserForm } from "./views/UserForm";
+import { User } from "./models/User";
+
+const user = User.buildUser({ name: "Clint", age: 35 });
+
+const root = document.getElementById("root");
+
+if (root) {
+  const userForm = new UserForm(root, user);
+  userForm.render();
+} else {
+  throw new Error("root element not found");
+}
