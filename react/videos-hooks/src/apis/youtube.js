ee759235e79478cacf7f7bf888e59f8c13a89034
ClintMI <ce@buildmidwestern.com>
2022-01-04 10:38:44 -0600

    Section 14 of react with stephen

--- react/videos-hooks/src/apis/youtube.js
+++ react/videos-hooks/src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = "AIzaSyBf-mBYkmKoyfGEBInvrtMxeZuMQBQGiho";
+const KEY = "AIzaSyD_Cba-YLc0ykxkSlN-OhN_vJIwNKKUA6Y";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
