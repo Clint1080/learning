56d1ceefcd0b28cf723eee08b8b98f80b26c97ac
ClintMI <ce@buildmidwestern.com>
2022-01-19 10:48:12 -0600

    I didn't finish out the streaming part

--- react/streams/client/src/actions/index.js
+++ react/streams/client/src/actions/index.js
@@ -1,5 +1,13 @@
 import streams from "../apis/streams";
-import { SIGN_IN, SIGN_OUT } from "./types";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+} from "./types";
 
 export const signIn = (userId) => {
   return {
@@ -15,5 +23,31 @@ export const signOut = () => {
 };
 
 export const createStream = (formValues) => async (dispatch) => {
-  streams.post("/streams", formValues);
+  const { data } = await streams.post("/streams", formValues);
+
+  dispatch({ type: CREATE_STREAM, payload: data });
+};
+
+export const fetchStreams = () => async (dispatch) => {
+  const { data } = await streams.get("/streams");
+
+  dispatch({ type: FETCH_STREAMS, payload: data });
+};
+
+export const fetchStream = (streamId) => async (dispatch) => {
+  const { data } = await streams.get(`streams/${streamId}`);
+
+  dispatch({ type: FETCH_STREAM, payload: data });
+};
+
+export const editStream = (id, formValues) => async (dispatch) => {
+  const { data } = await streams.put(`streams/${id}`, formValues);
+
+  dispatch({ type: EDIT_STREAM, payload: data });
+};
+
+export const deleteStream = (id) => async (dispatch) => {
+  await streams.delete(`streams/${id}`);
+
+  dispatch({ type: DELETE_STREAM, payload: id });
 };
--- react/streams/client/src/reducers/index.js
+++ react/streams/client/src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from "redux";
-import { reducer as fromReducer } from "redux-form";
+import { reducer as formReducer } from "redux-form";
 import authReducer from "./authReducer";
 
 export default combineReducers({
   auth: authReducer,
-  form: fromReducer,
+  form: formReducer,
 });
